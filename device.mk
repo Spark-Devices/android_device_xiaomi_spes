63f4f78c0ac947e11544cbd5a1aa80247ad09345
hsx02 <hsx02self@gmail.com>
2022-04-30 08:55:41 +0530

    spes: prebuilts: shipped with GoogleCameraGo
    modded by shamim dev
    Signed-off-by: hsx02 <hsx02self@gmail.com>

--- device.mk
+++ device.mk
@@ -233,6 +233,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor
 
+# GoogleCameraGo
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.spes
