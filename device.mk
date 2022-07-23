75192faf819eac589c465c648f5d62fb287e071d
Jabiyeff <cebiyevanar@gmail.com>
2022-07-23 12:43:39 +0400

    Revert "spes: Add KeyHandler to handle fingerprint key event"
    This reverts commit d719104eeecfa27ef4bca41074856bbfc41fa921.

--- device.mk
+++ device.mk
@@ -305,10 +305,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# KeyHandler
-PRODUCT_PACKAGES += \
-    KeyHandler
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.spes
